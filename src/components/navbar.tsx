9981e15ff764fda6ee55124945dc87d1ae77d686
virtuezzysm202 <162703883+virtuezzysm202@users.noreply.github.com>
2025-06-08 18:59:26 +0700

    UPDATE & FIX  NAVBAR

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -21,6 +21,7 @@ const Navbar = () => {
             <div className="flex items-center">
               <NavigationMenu>
                 <NavigationMenuList>
+                  {/* Home */}
                   <NavigationMenuItem>
                     <NavigationMenuLink
                       href="/"
@@ -29,12 +30,34 @@ const Navbar = () => {
                       Home
                     </NavigationMenuLink>
                   </NavigationMenuItem>
+
+                  {/* Articles */}
+                  <NavigationMenuItem>
+                    <NavigationMenuLink
+                      href="/artikel"
+                      className="group inline-flex h-10 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-muted hover:text-accent-foreground"
+                    >
+                      Articles
+                    </NavigationMenuLink>
+                  </NavigationMenuItem>
+
+                  {/* Reminder */}
                   <NavigationMenuItem>
                     <NavigationMenuLink
                       href="/reminder"
                       className="group inline-flex h-10 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-muted hover:text-accent-foreground"
                     >
                       Reminder
+                    </NavigationMenuLink>
+                  </NavigationMenuItem>
+
+                  {/* Workout */}
+                  <NavigationMenuItem>
+                    <NavigationMenuLink
+                      href="/workout-categories"
+                      className="group inline-flex h-10 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-muted hover:text-accent-foreground"
+                    >
+                      Workout
                     </NavigationMenuLink>
                   </NavigationMenuItem>
                 </NavigationMenuList>
