92f80ea90e29c21af59d53f73f6b8a76e5e250c9
virtuezzysm202 <162703883+virtuezzysm202@users.noreply.github.com>
2025-06-08 17:50:55 +0700

    Fix bug category name

--- src/pages/article/ArticleDetail.jsx
+++ src/pages/article/ArticleDetail.jsx
@@ -25,12 +25,15 @@ const ArticleDetail = () => {
       <div className="max-w-3xl mx-auto p-4">
         <img src={article.image_url} alt={article.title} className="w-full h-64 object-cover rounded-xl mb-4" />
         <h1 className="text-3xl font-bold mb-2">{article.title}</h1>
-        <p className="text-sm text-gray-500 mb-2">
-          Kategori: {article.category_name} | Penulis: {article.author_name}
-        </p>
-        <p className="text-sm text-gray-500 mb-6">
-          Tanggal Publikasi: {article.published_date}
-        </p>
+        <div className="bg-gray-100 p-4 rounded-lg shadow-sm mb-4">
+  <p className="text-sm font-semibold text-gray-800">
+    Kategori: {article.category_name} | Penulis: {article.author_name}
+  </p>
+  <p className="text-sm font-semibold text-gray-700 mt-1">
+    Tanggal Publikasi: {new Date(article.published_date).toLocaleDateString()}
+  </p>
+</div>
+
         <p className="text-lg leading-relaxed">{article.content}</p>
       </div>
     </>
